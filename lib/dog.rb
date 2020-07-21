269a89f38b84ac9e0ef36fdc0b4405c6502ffc9f
Kinnari Shah <kinnari1022@yahoo.com>
2020-07-21 21:45:05 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,9 +3,9 @@ class Dog
  def initialize(name, breed) 
    @name = name
    @breed = breed
-    if breed = nil 
-      breed = "Mutt"
-    end
+    # if breed = nil 
+    #   breed = "Mutt"
+    # end
    
  end
  
